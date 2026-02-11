810dba7904b69f8247b6f9054cdb89b6c8110882
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-11 17:26:41 +0200

    Essaie

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    // Cardinalité pour chaque service
+    public function hopital() {
+    return $this->belongsTo(Hopital::class);
+}
+
+public function tickets() {
+    return $this->hasMany(Ticket::class);
+}
 }
