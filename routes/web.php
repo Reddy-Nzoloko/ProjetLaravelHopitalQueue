0ad880ec1cf354702d2895982be4dd81eae08718
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-17 10:53:06 +0200

    Mise à jour du web

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ Route::prefix('admin')->group(function () {
     Route::get('/hopitaux/creer', [HopitalController::class, 'create'])->name('hopitaux.create');
     Route::post('/hopitaux', [HopitalController::class, 'store'])->name('hopitaux.store');
 });
-
 // route pour guichet
 use App\Http\Controllers\TicketController;
 Route::post('/ticket/generer', [TicketController::class, 'store'])->name('ticket.store');
