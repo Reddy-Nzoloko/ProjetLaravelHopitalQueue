5d6abddacc900f74cf83e1857b41c33b9f48bd4f
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-19 21:22:48 +0200

    cretion d'un super agent

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,31 +13,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-  public function run(): void
+public function run(): void
 {
-    // 1. Créer un hôpital de test
-    $hopital = \App\Models\Hopital::create([
-        'nom' => 'Hôpital Général',
-        'code_unique' => 'HG-001',
-        'adresse' => 'Butembo, RDC'
-    ]);
-
-    // 2. Créer un Admin pour cet hôpital
     \App\Models\User::create([
-        'name' => 'Admin Hopital',
-        'email' => 'admin@hopital.com',
-        'password' => bcrypt('password'),
-        'role' => 'admin_hopital',
-        'hopital_id' => $hopital->id,
-    ]);
-
-    // 3. Créer un Super Admin
-    \App\Models\User::create([
-        'name' => 'Super Admin',
-        'email' => 'super@system.com',
-        'password' => bcrypt('password'),
-        'role' => 'super_admin',
-        'hopital_id' => null,
+        'name' => 'Ton Nom',
+        'email' => 'admin@test.com',
+        'password' => bcrypt('password'), // Ton mot de passe sera 'password'
+        'role' => 'admin_global',        // Très important pour ton ServiceController
     ]);
 }
 }
