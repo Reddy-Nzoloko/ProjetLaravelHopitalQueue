062400dc0127185352b9c98a71d1b74773000c86
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-19 21:21:00 +0200

    installation de mon npm

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HopitalController;
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\TicketController;
 
-// --- GROUPE ADMIN ---
-Route::prefix('admin')->group(function () {
-    // Hôpitaux
-    Route::get('/hopitaux', [HopitalController::class, 'index'])->name('hopitaux.index');
-    Route::get('/hopitaux/creer', [HopitalController::class, 'create'])->name('hopitaux.create');
-    Route::post('/hopitaux', [HopitalController::class, 'store'])->name('hopitaux.store');
-
-    // Services
-    Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
-    Route::get('/services/creer', [ServiceController::class, 'create'])->name('services.create');
-    Route::post('/services', [ServiceController::class, 'store'])->name('services.store');
-
-    // Liste des tickets (Gestion des Appels)
-    Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// --- PARTIE BORNE (PATIENTS) ---
-Route::get('/borne/{hopital_id}', [TicketController::class, 'create'])->name('ticket.borne');
-Route::post('/ticket/generer', [TicketController::class, 'store'])->name('ticket.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', function () { return view('welcome'); });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
