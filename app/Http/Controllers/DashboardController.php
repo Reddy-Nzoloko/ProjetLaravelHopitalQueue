4b4f6ad5bb09a27d43f74893f652c057e49f3864
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-22 20:29:26 +0200

    Okay

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,21 +11,21 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-   public function index()
-{
-    $user = auth()->user();
+    public function index()
+    {
+        $user = Auth::user();
 
-    // Stats de base
-    $stats = [
-        'total_hopitaux' => \App\Models\Hopital::count(),
-        'total_services' => \App\Models\Service::count(),
-        'total_users'    => \App\Models\User::count(),
-        'total_tickets'  => \App\Models\Ticket::count(),
-    ];
+        // Stats de base
+        $stats = [
+            'total_hopitaux' => Hopital::count(),
+            'total_services' => Service::count(),
+            'total_users'    => User::count(),
+            'total_tickets'  => Ticket::count(),
+        ];
 
-    // On récupère TOUS les hôpitaux pour l'Admin Global
-    $hopitaux = \App\Models\Hopital::withCount('services')->get();
+        // On récupère TOUS les hôpitaux pour l'Admin Global
+        $hopitaux = Hopital::withCount('services')->get();
 
-    return view('dashboard', compact('stats', 'hopitaux'));
-}
+        return view('dashboard', compact('stats', 'hopitaux'));
+    }
 }
