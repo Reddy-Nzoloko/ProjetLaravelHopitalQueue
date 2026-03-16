7eeea84d743d0e588bf338da35e6aca4633d8c64
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-03-16 21:05:11 +0200

    Patient

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     use WithoutModelEvents;
 
     /**
-     * Seed the application's database. Création de l'administrateur 
+     * Seed the application's database. Création de l'espace patient
      */
 public function run(): void
 {
